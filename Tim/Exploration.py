58b614984a974a1b7c797d9b3f2b4f2d274c87f2
tveldema <tr.veldema@student.han.nl>
2025-09-04 11:56:06 +0200

    Starting work on fixing the U-Time dependencies

--- Tim/Exploration.py
+++ Tim/Exploration.py
@@ -1,9 +1,12 @@
 import mne
 import numpy as np
 import matplotlib.pyplot as plt
+from mne.time_frequency import tfr_multitaper
 
 file = mne.io.read_raw_edf("SC4001E0-PSG.edf")
 annotations = mne.read_annotations("SC4001EC-Hypnogram.edf")
+print(file.ch_names)
+print(file.get_channel_types())
 file.set_annotations(annotations)
 file.set_channel_types({
     'EEG Fpz-Cz': 'eeg',
@@ -15,42 +18,57 @@ file.set_channel_types({
     'Event marker': 'stim'
 })
 
-scalings = {
-    'eeg': 200e-6,
-    'eog': 300e-6,
-    'emg': 1e-3,
-    'misc': 0.5,
-    'stim': 100
-}
-# print(file.get_channel_types())
+file.pick_types(eeg=True)
+# Create epochs (30s windows)
+epochs = mne.make_fixed_length_epochs(file, duration=30, preload=True)
+
+# Compute average across epochs
+evoked = epochs.average()
+
+power = tfr_multitaper(epochs, freqs=np.arange(1, 40, 2),
+                       n_cycles=2, return_itc=False)
+
+# Plot power over time for a single channel
+power.plot(picks='EEG Fpz-Cz', baseline=(None, 0), mode='logratio')
+
+# Topographic plot at specific frequency
+power.plot_topomap(fmin=12, fmax=15, tmin=0, tmax=2, ch_type='eeg')
+# scalings = {
+#     'eeg': 1e-4,
+#     'eog': 1e-4,
+#     'emg': 1e-3,
+#     'misc': 0.5,
+#     'stim': 100
+# }
+#
 
 # file.plot(duration=10, scalings=scalings, show_scrollbars=True)
 
-annotations = file.annotations
-
-stage_mapping = {
-    "Sleep stage W": 0,
-    "Sleep stage 1": 1,
-    "Sleep stage 2": 2,
-    "Sleep stage 3": 3,
-    "Sleep stage 4": 3,
-    "Sleep stage R": 4,
-    "Sleep stage ?": 5
-}
-
-onsets = annotations.onset
-durations = annotations.duration
-stages = [stage_mapping[d] for d in annotations.description]
-
-sfreq = file.info['sfreq']
-end_time = file.times[-1]
-times = np.arange(0, end_time, 1/sfreq)
-
-stage_vector = np.zeros_like(times)
-
-for onset, duration, stage in zip(onsets, durations, stages):
-    idx = np.where((times >= onset) & (times < onset + duration))
-    stage_vector[idx] = stage
+# annotations = file.annotations
+#
+# stage_mapping = {
+#     "Sleep stage W": 0,
+#     "Sleep stage 1": 1,
+#     "Sleep stage 2": 2,
+#     "Sleep stage 3": 3,
+#     "Sleep stage 4": 3,
+#     "Sleep stage R": 4,
+#     "Sleep stage ?": 5
+# }
+#
+# onsets = annotations.onset
+# durations = annotations.duration
+# stages = [stage_mapping[d] for d in annotations.description]
+#
+# sfreq = file.info['sfreq']
+# end_time = file.times[-1]
+# times = np.arange(0, end_time, 1/sfreq)
+#
+# stage_vector = np.zeros_like(times)
+#
+# for onset, duration, stage in zip(onsets, durations, stages):
+#     idx = np.where((times >= onset) & (times < onset + duration))
+#     stage_vector[idx] = stage
 
 
 # plt.figure(figsize=(15, 3))
@@ -61,13 +79,12 @@ for onset, duration, stage in zip(onsets, durations, stages):
 # plt.title("Hypnogram")
 # plt.show()
 
-channels = mne.pick_types(file.info, eeg=True, eog=True)
+# channels = mne.pick_types(file.info, eeg=True, eog=True)
 
 file.plot(
-    duration=60,
-    scalings=scalings,
-    n_channels = len(channels),
-    picks=channels,
+    duration=30,
     show=True,
     block=True
 )
+
+
