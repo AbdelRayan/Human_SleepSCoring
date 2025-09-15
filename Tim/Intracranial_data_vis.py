ad0f62d2ef936d0c77f478756a112795ad146b7a
tveldema <tr.veldema@student.han.nl>
2025-09-15 14:40:06 +0200

    Created a script to automate file conversion, also moved the converter to a package folder

--- Tim/Intracranial_data_vis.py
+++ Tim/Intracranial_data_vis.py
@@ -6,17 +6,45 @@ import numpy as np
 from mne.export import export_raw
 import pandas as pd
 
-# files = [
-#     "D:/converted_sleep_data/2/con_full/2_night1_01.vhdr",
-#     "D:/converted_sleep_data/2/con_full/2_night1_02.vhdr",
-#     "D:/converted_sleep_data/2/con_full/2_night1_03.vhdr"
-# ]
-#
-# raw_list = [mne.io.read_raw_brainvision(f, preload=True) for f in files]
-#
-# raw = mne.concatenate_raws(raw_list)
+files = [
+    "D:/converted_sleep_data/2/con_full/2_night1_01.vhdr",
+    "D:/converted_sleep_data/2/con_full/2_night1_02.vhdr",
+    "D:/converted_sleep_data/2/con_full/2_night1_03.vhdr"
+]
 
-raw = mne.io.read_raw_brainvision("D:/converted_sleep_data/2/con_full/2_night1_02.vhdr")
+raw_list = [mne.io.read_raw_brainvision(f, preload=True) for f in files]
+
+raw = mne.concatenate_raws(raw_list)
+
+scores_files = [
+    "D:/converted_sleep_data/2/stages/2_night1_01_hypnogram.npy",
+    "D:/converted_sleep_data/2/stages/2_night1_02_hypnogram.npy",
+    "D:/converted_sleep_data/2/stages/2_night1_03_hypnogram.npy"
+]
+
+score_list = [np.load(f) for f in scores_files]
+hypno = np.concatenate(score_list)
+
+# raw = mne.io.read_raw_brainvision("D:/converted_sleep_data/2/con_full/2_night1_03.vhdr")
+
+# raw = mne.io.read_raw_edf("2_night1_03.edf")
+# hypno = np.load("D:/converted_sleep_data/2/stages/2_night1_03_hypnogram.npy")
+hypno = hypno.astype(int)
+eeg_duration = raw.n_times / raw.info['sfreq']
+n_epochs = int(eeg_duration // (30/4))  # assuming 30s epochs
+hypno = hypno[:n_epochs]            # crop to EEG length if needed
+stage_mapping = {0: "Wake", 1: "N1", 2: "N2", 3: "N3", 4: "REM"}
+description = [stage_mapping[s] for s in hypno]
+onset = np.arange(len(hypno)) * 30/4  # 30s per epoch
+duration = np.repeat(30/4, len(hypno))
+
+annotations = mne.Annotations(onset, duration, description)
+raw.set_annotations(annotations)
+raw.plot(duration=300,
+    scalings=dict(eeg=1e-4),  # adjust scaling if needed
+    block=True                # interactive plotting
+)
+# raw = mne.io.read_raw_brainvision("D:/converted_sleep_data/2/con_full/2_night1_02.vhdr")
 # raw_full.save("night1_full-raw.fif", overwrite=True)
 #
 # out_dir = "D:/converted_sleep_data/2/con_full/edf/"
@@ -40,14 +68,7 @@ raw = mne.io.read_raw_brainvision("D:/converted_sleep_data/2/con_full/2_night1_0
 
 #
 #
-# scores_files = [
-#     "D:/converted_sleep_data/2/stages/2_night1_01_hypnogram.npy",
-#     "D:/converted_sleep_data/2/stages/2_night1_02_hypnogram.npy",
-#     "D:/converted_sleep_data/2/stages/2_night1_03_hypnogram.npy"
-# ]
-#
-# score_list = [np.load(f) for f in scores_files]
-# full_score = np.concatenate(score_list)
+
 #
 
 # np.save("D:/converted_sleep_data/2/stages/night1_full_score.npy", full_score)
@@ -128,7 +149,7 @@ raw.set_channel_types({
 
 # raw.export("D:/converted_sleep_data/2/edf/2_night1_02.edf", fmt='edf', physical_range=(-200, 200))
 
-# raw.plot(block=True, scalings=dict(eeg=1e-4))
+
 
 # raw = mne.io.read_raw_edf("D:/converted_sleep_data/2/edf/night1_full.edf")
 #
@@ -141,9 +162,9 @@ raw.set_channel_types({
 
 # Number of samples (time points)
 # print("Samples:", raw.n_times)
-scores = "D:/converted_sleep_data/2/stages/2_night1_02_hypnogram.npy"
-#
-hypno = np.load(scores)
+
+
+
 # chan = raw.ch_names
 # sf = raw.info["sfreq"]
 # data = raw.get_data(picks="eeg", units="uV")
@@ -151,7 +172,7 @@ hypno = np.load(scores)
 # print(len(hypno))
 # print(np.unique(hypno))
 # print(chan)
-hypno_up = yasa.hypno_upsample_to_data(hypno, sf_hypno=4/30, data=raw)
+# hypno_up = yasa.hypno_upsample_to_data(hypno, sf_hypno=4/30, data=raw)
 # yasa.plot_spectrogram(data[chan.index("Cz")], sf, hypno_up)
 # plt.show(block=True)
 # yasa.plot_hypnogram(hypno)
@@ -167,3 +188,5 @@ hypno_up = yasa.hypno_upsample_to_data(hypno, sf_hypno=4/30, data=raw)
 # plt.ylabel("Delta power (µV²/Hz)")
 # plt.xlabel("Channels")
 # plt.show()
+
+raw.plot(duration=800, scalings=dict(eeg=1e-4), block=True, )
